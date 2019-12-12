94a5559c6d4040cd7588dbdad51754134bbc0a43
rafaelreis <rafaelreis@localhost.localdomain>
2019-12-12 19:03:53 -0300

    feat(App): tmp main page with particles and blaze sounds

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import Particles from 'react-particles-js';
+import config from './assets/particle';
+import blaze from './assets/blaze.mp3'
 import './App.css';
 
-import Home from './pages/Home';
+import Sound from 'react-sound';
+
+//import Home from './pages/Home';
 
 function App() {
-    return (
-        <Home/>
+    return ([
+        <Particles
+            params={config}
+        />,
+        <Sound
+            url={blaze}
+            playStatus={Sound.status.PLAYING}
+            loop={true}
+            // onLoading={Sound.handleSongLoading}
+            // onPlaying={Sound.handleSongPlaying}
+            // onFinishedPlaying={Sound.status.PLAYING}
+        />]
     );
 }
 
